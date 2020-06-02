2984aab2898114ac1fb19d7e692d4d1076e68931
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-06-02 21:06:02 +0530

    Add styles to host

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: "Neat Tree",
+      username: "nature",
+      content: "I saw this neat tree today",
+      imageUrl: "assets/tree.jpeg",
+    },
+    {
+      title: "Snowy Mountain",
+      username: "mountainlover",
+      content: "Here is a picture of a snowy mountain",
+      imageUrl: "assets/mountain.jpeg",
+    },
+    {
+      title: "Mountain Biking",
+      username: "biking1232",
+      content: "I did some biking today",
+      imageUrl: "assets/biking.jpeg",
+    },
+  ];
 }
